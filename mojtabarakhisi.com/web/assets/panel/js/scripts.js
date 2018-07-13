01ec8c6a9aa2cd641f7404a567918bb09ed52117
mojtabarks <mojtaba.street@gmail.com>
2018-07-13 20:19:07 +0430

    drop symfony

--- mojtabarakhisi.com/web/assets/panel/js/scripts.js
+++
@@ -1,8 +0,0 @@
-$('#myframe').on('load', function () {
-    $(this).contents().on('click','.select',function () {
-        var path = $(this).attr('data-path')
-        $('#path').val(path);
-        $('#image').attr('src', path)
-        $('#myModal').modal('hide')
-    });
-});
