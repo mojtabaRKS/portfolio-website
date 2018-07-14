4374a1477386ec53b297b9819f3bd87395420bd2
mojtabarks <mojtaba.street@gmail.com>
2018-07-15 01:11:40 +0430

    adding image - skill relationship

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -9,4 +9,12 @@ class Skill extends Model
     protected $fillable = [
         'name' , 'description' , 'tools' , 'percent' , 'is_active' , 'file_id'
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function image()
+    {
+        return $this->belongsTo(File::class);
+    }
 }
