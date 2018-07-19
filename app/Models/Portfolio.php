e99653fe60104c8cc2c12521354ce286eaacbe8d
mojtabarks <mojtaba.street@gmail.com>
2018-07-19 19:39:55 +0430

    relations in portfolio model

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portfolio extends Model
 {
-    //
+    protected $fillable = [
+        'name' , 'description' , 'link' , 'file_id'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function file()
+    {
+        return $this->belongsTo(File::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function attributes()
+    {
+        return $this->belongsToMany(Attribute::class);
+    }
 }
