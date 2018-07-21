e4a53dbef483913998c0043c7a518c74c0a4b16d
mojtabarks <mojtaba.street@gmail.com>
2018-07-21 19:39:17 +0430

    panel select active nav links

--- resources/views/panel/layouts/sidebar.blade.php
+++ resources/views/panel/layouts/sidebar.blade.php
@@ -11,30 +11,53 @@
     </div>
     <div class="sidebar-wrapper">
         <ul class="nav">
-            <li class="nav-item active  ">
+            <li class="nav-item {{ (!str_contains(request()->path() , ['skills' , 'portfolios' , 'attributes'])) ? 'active' : '' }} ">
                 <a class="nav-link" href="{{ route('dashboard') }}">
                     <i class="material-icons">dashboard</i>
                     <p>Dashboard</p>
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('skills.index') }}">
-                    <i class="material-icons">fitness_center</i>
-                    <p>Skills</p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('portfolios.index') }}">
-                    <i class="material-icons">assignment_ind</i>
-                    <p>Portfolios</p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('attributes.index') }}">
-                    <i class="material-icons">extension</i>
-                    <p>Attributes</p>
-                </a>
-            </li>
+            @if(request()->user()->hasRole('admin'))
+                <li class="nav-item {{ (str_contains(request()->path() , 'skills')) ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('skills.index') }}">
+                        <i class="material-icons">fitness_center</i>
+                        <p>Skills</p>
+                    </a>
+                </li>
+                <li class="nav-item {{ (str_contains(request()->path() , 'portfolios')) ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('portfolios.index') }}">
+                        <i class="material-icons">assignment_ind</i>
+                        <p>Portfolios</p>
+                    </a>
+                </li>
+                <li class="nav-item {{ (str_contains(request()->path() , 'attributes')) ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('attributes.index') }}">
+                        <i class="material-icons">extension</i>
+                        <p>Attributes</p>
+                    </a>
+                </li>
+            @endif
+
+            @if(request()->user()->hasRole('user'))
+                <li class="nav-item">
+                    <a class="nav-link" href="">
+                        <i class="material-icons">fitness_center</i>
+                        <p>Skills</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">
+                        <i class="material-icons">assignment_ind</i>
+                        <p>Portfolios</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">
+                        <i class="material-icons">extension</i>
+                        <p>Attributes</p>
+                    </a>
+                </li>
+            @endif
         </ul>
     </div>
 </div>
